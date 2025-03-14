5694bb42647cceb53b2dc50d6fb59f7a4ee8003c
Uvesh99 <sayaidisrar@gmail.com>
2025-03-14 17:29:25 +0530

    Minor bug resolved

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -4,7 +4,7 @@ class ApIError extends Error{
         statusCode,
         message="Something went wrong",
         errors=[],
-        statck=""
+        stack=""
     ){
         super(message)
         this.statusCode=statusCode
@@ -13,8 +13,8 @@ class ApIError extends Error{
         this.success=false;
         this.errors=errors
 
-        if(statck){
-            this.stack=statck
+        if(stack){
+            this.stack=stack
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
